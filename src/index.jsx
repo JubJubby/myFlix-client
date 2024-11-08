6ff690fa683f8644456e71dbdec5ecb4d2406c2b
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-11-08 09:55:34 -0500

    import and change return statement to the MainView

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,12 @@
 import {createRoot} from "react-dom/client";
+import {MainView} from "./components/main-view/main-view";
 
 // Import statement to indicate the need to bundle `./index.scss`
 import "./index.scss";
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-    return (
-        <div className="myFlix">
-            <div>Good Morning</div>
-        </div>
-    );
+    return <MainView />;
 };
 
 // Finds the root
