5ebb80d978d284a94acd7a3b5dea5acd615c35df
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-11-13 11:15:39 -0500

    install and import prop-types into movie-card

--- components/movie-card/movie-card.jsx
+++ components/movie-card/movie-card.jsx
@@ -1,12 +1,17 @@
+import PropTypes from "prop-types";
+
+//MovieCard function component
 export const MovieCard = ({movie, onMovieClick}) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
-    );
-  };
-  
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}
+    </div>
+  );
+};
+
+//Defining prop constraints
+
