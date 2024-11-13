763ec5ef7a3e09a12c67d8cdfe7e781839e97816
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-11-13 11:17:08 -0500

    populate movies state using setMovies from movies_api

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -1,39 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Inception",
-      description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
-      genre: "Sci-Fi",
-      image:
-        "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRRqhsSA3e6Kw-EAbjneujrnhXKq7Ugl5L5Wh0yyLC92qimzfJw",
-      director: "Christopher Nolan",
-    },
-    {
-        id: 2,
-        title: "The Shawshank Redemption",
-        description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through.",
-        genre: "Genre",
-        image:
-          "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTRQY22fAfc4eI-8Fpg12E7d7O68wz0uqJvXHunh8l3Xbg1T6Np",
-        director: "Frank Darabont",
-      },
-      {
-        id: 3,
-        title: "Gladiator",
-        description: "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
-        genre: "Adventure",
-        image:
-          "https://m.media-amazon.com/images/I/51GA6V6VE1L.jpg",
-        director: "Ridley Scott",
-      },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jub-flix-e9807f9b5fd0.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const MoviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.Title,
+            image: doc.ImagePath,
+            description: doc.Description,
+            genre: doc.Genre,
+            director: doc.Director,
+            actors: doc.Actors
+          };
+        });
+
+        setMovies(MoviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
