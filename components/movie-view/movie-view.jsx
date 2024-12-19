cbcc884a1cd2b038d3c17f0f1422feae23dacf28
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-12-19 12:18:17 -0500

    formatting

--- components/movie-view/movie-view.jsx
+++ components/movie-view/movie-view.jsx
@@ -1,27 +1,26 @@
 export const MovieView = ({movie, onBackClick}) => {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <img src={movie.image} />
-        </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.title}</span>
-        </div>
-        <div>
-          <span>Description: </span>
-          <span>{movie.discription}</span>
-        </div>
-        <div>
-          <span>Genre: </span>
-          <span>{movie.genre}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movie.director}</span>
-        </div>
-        <button onClick={onBackClick}>Back</button>
+        <img src={movie.image} />
       </div>
-    );
-  };
-  
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.discription}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.genre}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  );
+};
