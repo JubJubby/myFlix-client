25ff03f896abb3c4df4fe91e36aea3762cb5479d
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2025-01-07 14:56:07 -0500

    create profile-view.jsx and .scss, then add code from signup-view with tweaks and add user's list of favorite movies

---
+++ components/profile-view/profile-view.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { MovieCard } from "../movie-card/movie-card";
+import "./profile-view.scss";
+
+export const ProfileView = () => {
+    const localUser = JSON.parse(localStorage.getItem("user"));
+    const favoriteMovies = movies.filter((movie) => {
+        return localUser.FavoriteMovies.includes(movie._id);
+    });
+
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [birthday, setBirthday] = useState("");
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+  
+      const data = {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday,
+      };
+  
+      fetch("https://jub-flix-e9807f9b5fd0.herokuapp.com/users", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) => {
+        if (response.ok) {
+          alert("Singup successful");
+          window.location.reload();
+        } else {
+          alert("Signup failed");
+        }
+      });
+    };
+  
+    return (
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="3"
+            placeholder="Enter a username"
+          />
+        </Form.Group>
+  
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            minLength="8"
+            placeholder="Enter a password"
+          />
+        </Form.Group>
+  
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            placeholder="Enter an email"
+          />
+        </Form.Group>
+  
+        <Form.Group controlId="formBirthday">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control
+            type="date"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        {
+            localUser && favoriteMovies.Map((movie) => (
+                <MovieCard movie={movie} />
+            ))
+        }
+      </Form>
+    );
+  };
+  
